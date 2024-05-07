0309eb81358bb81ad3fb14e48551be52b15f6dc8
CETECOM <cetecom_conce@duoc.cl>
2024-05-07 12:02:45 -0400

    apifuncional

--- ProyectoAgua/java/map.js
+++ ProyectoAgua/java/map.js
@@ -1,11 +1,13 @@
-function iniciarMap(){
-    var coord = {lat:-34.5956145 ,lng: -58.4431949};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-}
+// Initialize the map
+const map = L.map('map').setView([-45.459908, -72.814236], 20);
+
+// Add a tile layer from OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Add a marker to the map
+const marker = L.marker([-45.459908, -72.814236]).addTo(map);
+
+// Center the map on the marker
+map.setView(marker.getLatLng(), 20);
